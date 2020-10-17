71e71daea95cb21715fcf1caba271c83a87bd5a3
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-10-17 16:21:50 +0200

    Verbose settings

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,17 @@ function createSynologyChatFileService() {
 	const port = <any>process.env.PORT || 8033;
 	const baseUrl = process.env.FILE_SERVER_BASE_URL || "http://127.0.0.1";
 	const incomingUrl = process.env.SYNOLOGY_CHAT_INCOMING_URL;
-	const verbose = process.env.VERBOSE in ["1", "true", 1, true];
+
+	let verbose = true;
+
+	if (process.env.VERBOSE) {
+		if (process.env.VERBOSE in ["1", "true", "TRUE"]) {
+			verbose = true;
+		}
+		else if (process.env.VERBOSE in ["0", "false", "FALSE"]) {
+			verbose = false;
+		}
+	}
 
 	return new SynologyChatFileService(port, baseUrl, incomingUrl, verbose);
 }
