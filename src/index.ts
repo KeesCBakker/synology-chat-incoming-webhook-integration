aba75e791312bd5f62074b754ef6d3de9f79726c
Kees C. Bakker <k.bakker@hostedsolutions.nu>
2020-10-17 16:22:52 +0200

    Port number

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { SynoChatIncomingWebhookService } from './SynoChatService';
 
 function createSynologyChatFileService() {
 	const port = <any>process.env.PORT || 8033;
-	const baseUrl = process.env.FILE_SERVER_BASE_URL || "http://127.0.0.1";
+	const baseUrl = process.env.FILE_SERVER_BASE_URL || "http://127.0.0.1:" + port;
 	const incomingUrl = process.env.SYNOLOGY_CHAT_INCOMING_URL;
 
 	let verbose = true;
