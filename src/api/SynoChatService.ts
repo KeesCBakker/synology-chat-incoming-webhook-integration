09ddd7c836b139f7b6ed5841d3ffb88fab7aba6c
Kees C. Bakker <info@hostedsolutions.nu>
2021-09-25 10:51:08 +0200

    Upgrade packages.

--- src/api/SynoChatService.ts
+++ src/api/SynoChatService.ts
@@ -1,6 +1,7 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
-const INCOMING_URL_REGEX = /https?:\/\/.*?\/webapi\/entry\.cgi\?api=SYNO\.Chat\.External&method=incoming&version=2&token=%22.*%22/
+const INCOMING_URL_REGEX =
+	/https?:\/\/.*?\/webapi\/entry\.cgi\?api=SYNO\.Chat\.External&method=incoming&version=2&token=%22.*%22/;
 
 /**
  * Service that interacts with Synology Chat using the
@@ -10,7 +11,6 @@ const INCOMING_URL_REGEX = /https?:\/\/.*?\/webapi\/entry\.cgi\?api=SYNO\.Chat\.
  * @class SynoChatIncomingWebhookService
  */
 export class SynoChatIncomingWebhookService {
-
 	/**
 	 * Creates an instance of SynoChatIncomingWebhookService.
 	 * @param {string} incomingUrl The URL of the incoming webhook integration.
@@ -18,11 +18,10 @@ export class SynoChatIncomingWebhookService {
 	 * @memberOf SynoChatIncomingWebhookService
 	 */
 	constructor(private incomingUrl: string) {
-
 		if (!INCOMING_URL_REGEX.test(incomingUrl)) {
 			throw new Error(`Invalid incoming url, it should have this patern:
 {http or https}://{url or ip}/webapi/entry.cgi?api=SYNO.Chat.External&method=incoming&version=2&token=%22{your-token}%22,
-`)
+`);
 		}
 	}
 
@@ -35,9 +34,8 @@ export class SynoChatIncomingWebhookService {
 	 * @memberOf SynoChatIncomingWebhookService
 	 */
 	async send(text: string, fileUrl: string | null = null) {
-
 		const data = {
-			"text": text,
+			text: text,
 		} as any;
 
 		if (fileUrl) {
@@ -45,25 +43,26 @@ export class SynoChatIncomingWebhookService {
 		}
 
 		const response = await fetch(this.incomingUrl, {
-			method: 'post',
-			body: new URLSearchParams([
-				['payload', JSON.stringify(data)]
-			]),
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			method: "post",
+			body: new URLSearchParams([["payload", JSON.stringify(data)]]),
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
 		});
 
-		const answer = await response.json();
+		const answer: any = await response.json();
 		if (answer.error) {
-
 			let msg = "Error while comunicating with Synology Chat.";
-			msg += "\n" + JSON.stringify(answer.error)
+			msg += "\n" + JSON.stringify(answer.error);
 
-			if (answer.error.code == 117 && fileUrl && fileUrl.startsWith("http://127.0.0.1")) {
-				msg = "\nHint: when running locally make sure you use the IP of your computer in the SCIWI_FILE_SERVER_BASE_URL.";
+			if (
+				answer.error.code == 117 &&
+				fileUrl &&
+				fileUrl.startsWith("http://127.0.0.1")
+			) {
+				msg =
+					"\nHint: when running locally make sure you use the IP of your computer in the SCIWI_FILE_SERVER_BASE_URL.";
 			}
 
 			throw new Error(msg);
 		}
-
 	}
 }
